cf37aef2d87fef8b61f2ef966a9bf0d9a25a659a
lamar <liliulun@gmail.com>
2019-05-08 16:17:09 -0700

    style color slider

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]));
     return (
       <div>
         <Palette palette={generatePalette(seedColors[4])} />
