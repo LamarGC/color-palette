bd5160dba81812a3f90fe33cd633db808533fd9f
lamar <liliulun@gmail.com>
2019-05-14 16:52:33 -0700

    make paletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,5 +1,4 @@
-import { whileStatement } from "@babel/types";
-
+import sizes from "./sizes";
 export default {
   root: {
     backgroundColor: "blue",
@@ -13,14 +12,20 @@ export default {
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%"
+    },
+    [sizes.down("xs")]: {
+      width: "75%"
+    }
   },
   nav: {
     display: "flex",
     width: "100%",
     justifyContent: "space-between",
-    color: "white",
     alignItems: "center",
+    color: "white",
     "& a": {
       color: "white"
     }
@@ -29,7 +34,14 @@ export default {
     boxSizing: "border-box",
     width: "100%",
     display: "grid",
-    gridTemplateColumns: "repeat(3,30%)",
-    gridGap: "5%"
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)"
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1rem"
+    }
   }
 };
